6972d12daf9f3925893428e008fed83eab1aaa9a
Sam Poder <39828164+sampoder@users.noreply.github.com>
2020-01-27 11:48:05 +0000

    PEP 8pppppper

--- pistatus/__main__.py
+++ pistatus/__main__.py
@@ -5,19 +5,24 @@ import sys
 import tkinter as tk
 from flask import Flask, render_template
 
+
 def main():
 
     if len(sys.argv) == 1:
-        
-        hostname = re.sub('\n','',os.popen('hostname').read())
 
-        ip = re.sub('\n','',os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
+        hostname = re.sub('\n', '', os.popen('hostname').read())
+
+        ip = re.sub(
+            '\n', '',
+            os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
 
         memory = psutil.virtual_memory()
 
         cpuusage = psutil.cpu_percent(interval=1)
 
-        cputemp = re.sub('temp=','',re.sub('\n','',os.popen('vcgencmd measure_temp').read()))
+        cputemp = re.sub('temp=', '',
+                         re.sub('\n', '',
+                                os.popen('vcgencmd measure_temp').read()))
 
         disk = psutil.disk_usage('/')
 
@@ -25,19 +30,24 @@ def main():
 
         print("IP Address: " + ip + "\n")
 
-        print("Physical Memory: " + str(round(memory.total/1000000)) +" MB")
+        print("Physical Memory: " + str(round(memory.total / 1000000)) + " MB")
 
-        print("Available Memory: " + str(round(memory.available/1000000)) + " MB\n")
+        print("Available Memory: " + str(round(memory.available / 1000000)) +
+              " MB\n")
 
         print("CPU Usage: " + str(cpuusage) + "%")
 
         print("CPU Temperature: " + cputemp + "\n")
 
-        print("Disk Total Storage: " + str(round(disk.total/1000000000,2)) + " GB")
+        print("Disk Total Storage: " + str(round(disk.total / 1000000000, 2)) +
+              " GB")
 
-        print("Disk Available Storage: " + str(round(disk.free/1000000000,2)) + " GB \n" + "\n")
+        print("Disk Available Storage: " +
+              str(round(disk.free / 1000000000, 2)) + " GB \n" + "\n")
 
-        print("No Command Line Arguments Specified. Use -g to launch GUI app. Use -w to launch Web Server")
+        print(
+            "No Command Line Arguments Specified. Use -g to launch GUI app. Use -w to launch Web Server"
+        )
 
     elif sys.argv[1] == "-g":
 
@@ -48,25 +58,41 @@ def main():
 
         # Create label
         label = tk.Label(root, text="Loading")
-            # Lay out label
+        # Lay out label
         label.pack()
+
         def setText():
 
-            hostname = re.sub('\n','',os.popen('hostname').read())
+            hostname = re.sub('\n', '', os.popen('hostname').read())
 
-            ip = re.sub('\n','',os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
+            ip = re.sub(
+                '\n', '',
+                os.popen(
+                    'ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
 
             memory = psutil.virtual_memory()
 
             cpuusage = psutil.cpu_percent(interval=1)
 
-            cputemp = re.sub('temp=','',re.sub('\n','',os.popen('vcgencmd measure_temp').read()))
+            cputemp = re.sub('temp=', '',
+                             re.sub('\n', '',
+                                    os.popen('vcgencmd measure_temp').read()))
 
             disk = psutil.disk_usage('/')
-            
-            label.configure(text= "\n" + "Hostname: " + hostname + "\n" + "IP Address: " + ip + "\n" + "\n" + "Physical Memory: " + str(round(memory.total/1000000)) +" MB" + "\n" + "Available Memory: " + str(round(memory.available/1000000)) + " MB" +  "\n" + "\n" + "CPU Usage: " + str(cpuusage) + "%" + "\n" + "CPU Temperature: " + cputemp + "\n" + "\n" + "Disk Total Storage: " + str(round(disk.total/1000000000,2)) + " GB" + "\n" + "Disk Available Storage: " + str(round(disk.free/1000000000,2)) + " GB \n")
-            
-            root.after(10000,setText)
+
+            label.configure(
+                text="\n" + "Hostname: " + hostname + "\n" + "IP Address: " +
+                ip + "\n" + "\n" + "Physical Memory: " + str(
+                    round(memory.total / 1000000)) + " MB" + "\n" +
+                "Available Memory: " + str(round(memory.available / 1000000)) +
+                " MB" + "\n" + "\n" + "CPU Usage: " + str(
+                    cpuusage) + "%" + "\n" + "CPU Temperature: " + cputemp +
+                "\n" + "\n" + "Disk Total Storage: " + str(
+                    round(disk.total / 1000000000, 2)) + " GB" + "\n" +
+                "Disk Available Storage: " + str(
+                    round(disk.free / 1000000000, 2)) + " GB \n")
+
+            root.after(10000, setText)
 
         setText()
 
@@ -75,24 +101,44 @@ def main():
 
     elif sys.argv[1] == "-w":
 
-        app = Flask(__name__, template_folder=os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates'))
+        app = Flask(
+            __name__,
+            template_folder=os.path.join(
+                os.path.dirname(os.path.abspath(__file__)), 'templates'))
+
         @app.route('/')
         @app.route('/index')
         def index():
 
-            hostname = re.sub('\n','',os.popen('hostname').read())
+            hostname = re.sub('\n', '', os.popen('hostname').read())
 
-            ip = re.sub('\n','',os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
+            ip = re.sub(
+                '\n', '',
+                os.popen(
+                    'ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
 
             memory = psutil.virtual_memory()
 
             cpuusage = psutil.cpu_percent(interval=1)
 
-            cputemp = re.sub('temp=','',re.sub('\n','',os.popen('vcgencmd measure_temp').read()))
+            cputemp = re.sub('temp=', '',
+                             re.sub('\n', '',
+                                    os.popen('vcgencmd measure_temp').read()))
 
             disk = psutil.disk_usage('/')
 
-            return render_template('index.html', Title=hostname, ip = ip, pmemory = str(round(memory.total/1000000)), amemory = str(round(memory.available/1000000)), cpuusage = str(cpuusage), cputemp = cputemp, tdisk = str(round(disk.total/1000000000)),adisk = str(round(disk.free/1000000000,2)))
+            return render_template(
+                'index.html',
+                Title=hostname,
+                ip=ip,
+                pmemory=str(round(memory.total / 1000000)),
+                amemory=str(round(memory.available / 1000000)),
+                cpuusage=str(cpuusage),
+                cputemp=cputemp,
+                tdisk=str(round(disk.total / 1000000000)),
+                adisk=str(round(disk.free / 1000000000, 2)))
 
-        ip = re.sub('\n','',os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
+        ip = re.sub(
+            '\n', '',
+            os.popen('ip addr show wlan0 | grep -Po \'inet \K[\d.]+\'').read())
         app.run(host=ip)
--- setup.py
+++ setup.py
@@ -6,19 +6,18 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pistatus",
-    version="0.0.1.3",
+    version="0.0.1.4",
     author="Sam Poder",
     author_email="23samuel.p@gwa.edu.sg",
-    description="An application that allows you to check the status of your Raspberry Pi",
+    description=
+    "An application that allows you to check the status of your Raspberry Pi",
     long_description=long_description,
     long_description_content_type="text/markdown",
     packages=setuptools.find_packages(),
     include_package_data=True,
     entry_points={'console_scripts': ['pistatus=pistatus.__main__:main']},
     classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "License :: OSI Approved :: MIT License"
     ],
-    python_requires='>=3.6'
-)
+    python_requires='>=3.6')
